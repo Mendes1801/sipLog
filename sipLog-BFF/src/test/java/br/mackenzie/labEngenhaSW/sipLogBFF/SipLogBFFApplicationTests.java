210a09b808e7e088615526f22c8cc825e87bf310
Gabriel <gaerme@gmail.com>
2026-04-16 12:41:01 -0300

    fix test

--- sipLog-BFF/src/test/java/br/mackenzie/labEngenhaSW/sipLogBFF/SipLogBFFApplicationTests.java
+++ sipLog-BFF/src/test/java/br/mackenzie/labEngenhaSW/sipLogBFF/SipLogBFFApplicationTests.java
@@ -2,8 +2,10 @@ package br.mackenzie.labEngenhaSW.sipLogBFF;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SipLogBFFApplicationTests {
 
 	@Test
