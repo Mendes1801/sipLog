d089992d2fbdc48aacded1fec8fc05a031d37fb2
Gabriel <gaerme@gmail.com>
2026-04-16 12:46:48 -0300

    fix test2

--- core_api/apiCore-sipLog/src/test/java/br/mackenzie/labEngenhariaSW/sipLog/apiCore_sipLog/ApiCoreSipLogApplicationTests.java
+++ core_api/apiCore-sipLog/src/test/java/br/mackenzie/labEngenhariaSW/sipLog/apiCore_sipLog/ApiCoreSipLogApplicationTests.java
@@ -2,8 +2,10 @@ package br.mackenzie.labEngenhariaSW.sipLog.apiCore_sipLog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ApiCoreSipLogApplicationTests {
 
 	@Test
